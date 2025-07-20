da99577b6c7c76e6246a3b0396ca1fd7417dc4ac
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-07-21 01:45:18 +0900

    feat: (new) 컴포넌트 적용
    App 컴포넌트(부모 컴포넌트)에서 Header, Main, Footer 컴포넌트(자식 컴포넌트)를 불러와서 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-    return (
-        <>
-            <h1>Hello, React!</h1>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
