4101807cc60730ce7aad37c66ef631e36ca6c01c
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-07-21 02:16:11 +0900

    feat: 컴포넌트의 값으로 HTML, CSS 표현

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,19 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
+  const user = {
+    name: "2TS",
+    isLogin: false,
+  };
+
   return (
     <>
       <Header />
       <Main />
       <Footer />
+      <div>
+        {user.name} -&gt; {user.isLogin ? <span style={{ backgroundColor: "blue" }}>login</span> : <span style={{ backgroundColor: "red" }}>logout</span>}
+      </div>
     </>
   );
 }
