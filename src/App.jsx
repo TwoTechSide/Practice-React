0af301f4f3c814747c434a451fd54f124fe944f1
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-07-21 02:36:22 +0900

    feat: (new) Props로 데이터 전달

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Button from "./components/Button";
 
 function App() {
-  const user = {
-    name: "2TS",
-    isLogin: false,
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
   };
-
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
-      <div>
-        {user.name} -&gt; {user.isLogin ? <span style={{ backgroundColor: "blue" }}>login</span> : <span style={{ backgroundColor: "red" }}>logout</span>}
-      </div>
+      <Button {...buttonProps} />
+      <Button text={"카페"} />
+      <Button text={"블로그"}>
+        <Header />
+      </Button>
     </>
   );
 }
